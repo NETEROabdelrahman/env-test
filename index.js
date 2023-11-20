550eb6ae5d0fe9fc5401f56615c2fa3263922dfe
Abdelrahman Adel <gharibaboddahab@gmail.com>
2023-11-20 15:48:24 +0200

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-require('dotenv').config()
+const container = document.getElementById("0")
+const btn = document.getElementById("1")
+let containerWidth = container.offsetWidth;
+let windowWidth = window.innerWidth;
 
-console.log(process.env.SECRET)
+btn.style.left = `calc(9vmin + ${(windowWidth-containerWidth)/2}px)`
+
+window.addEventListener('resize', function () {
+    containerWidth = container.offsetWidth;
+    windowWidth = window.innerWidth;
+    btn.style.left = `calc(9vmin + ${(windowWidth-containerWidth)/2}px)`
+});
+  
+btn.onclick = (e) => {
+    e.preventDefault();
+    btn.classList.toggle("red")
+}
+
